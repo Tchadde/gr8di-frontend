69fe00ec6819987b306ebd08f84126b918a7ba97
kraulain <fosso@afgrey.com>
2018-02-05 23:05:10 +0100

    Fosso | added content for projects page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FootBarComponent } from './foot-bar/foot-bar.component';
 import { HeadBarComponent } from './head-bar/head-bar.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FootBarComponent,
-    HeadBarComponent
+    HeadBarComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ProjectsComponent]
 })
 export class AppModule { }
